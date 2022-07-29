dce525dfb26b378895a1f424fe5c812f58a136b6
soso <avilasofia076@gmail.com>
2022-07-29 14:04:49 -0300

    main

--- primerproyecto/src/main/java/com/miaplicacion/primerproyecto/Controller/HolaController.java
+++ primerproyecto/src/main/java/com/miaplicacion/primerproyecto/Controller/HolaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,7 +23,11 @@ public class HolaController {
         return "Hello world "+ nombre;
     }
     
-    @PostMapping ("/cliente")
+    /**
+     *
+     * @param cli
+     */
+    @PostMapping ("/hola")
     public void nuevoCliente(@RequestBody Cliente cli){
         System.out.println("Datos del cliente: "+ cli.getNombre() + " Apellido:  "+ cli.getApellido());
     }
