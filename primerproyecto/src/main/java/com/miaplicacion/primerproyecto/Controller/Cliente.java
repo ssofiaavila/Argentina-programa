7732f56ac5b828c0ce43d18afb1f36e81743dda3
soso <avilasofia076@gmail.com>
2022-07-28 23:21:33 -0300

    springboot

---
+++ primerproyecto/src/main/java/com/miaplicacion/primerproyecto/Controller/Cliente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.miaplicacion.primerproyecto.Controller;
+
+/**
+ *
+ * @author Sofia
+ */
+class Cliente {
+    private Long id;
+    private String nombre;
+    private String apellido;
+
+    String getNombre() {
+      return this.nombre;
+    }
+
+    String getApellido() {
+        return this.apellido;
+    }
+}
