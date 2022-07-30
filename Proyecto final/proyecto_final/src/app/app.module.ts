8b8e5bc49ca8b81047053b7c18fa1fe337f2f610
soso <avilasofia076@gmail.com>
2022-07-30 11:50:31 -0300

    commit

--- Proyecto final/proyecto_final/src/app/app.module.ts
+++ Proyecto final/proyecto_final/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 
@@ -16,7 +17,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AboutComponent
 
   ],
   imports: [
