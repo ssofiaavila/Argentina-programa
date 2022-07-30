95303dee8d37236aa4305dc71b2e3e9f8840d9b1
soso <avilasofia076@gmail.com>
2022-07-30 11:28:45 -0300

    banner

--- Proyecto final/proyecto_final/src/app/app.module.ts
+++ Proyecto final/proyecto_final/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 
 
@@ -14,7 +15,8 @@ import { SocialComponent } from './components/social/social.component';
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    SocialComponent
+    SocialComponent,
+    BannerComponent
 
   ],
   imports: [
