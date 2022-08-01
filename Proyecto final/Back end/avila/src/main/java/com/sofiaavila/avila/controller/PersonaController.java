69273eabfc9ac1bd8c77f25ff6a3b0fb1d6093c0
soso <avilasofia076@gmail.com>
2022-08-01 14:17:22 -0300

    Update PersonaController.java

--- Proyecto final/Back end/avila/src/main/java/com/sofiaavila/avila/controller/PersonaController.java
+++ Proyecto final/Back end/avila/src/main/java/com/sofiaavila/avila/controller/PersonaController.java
@@ -5,6 +5,12 @@ import com.sofiaavila.avila.Interface.IPersonaService;
 import com.sofiaavila.avila.entity.Persona;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +22,30 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
-    public 
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody Persona persona){
+        ipersonaService.savePersona(persona);
+        return "Persona creada correctamente";
+    }
+    
+    @DeleteMapping("/personas/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        ipersonaService.deletePersona(id);
+        return "Persona eliminada correctamente";
+    }
+    
+    @PostMapping("/personas/editar/{id}")
+    public Persona editPersona(@PathVariable Long id, @RequestParam("nombre")String nuevoNombre, 
+            @RequestParam("apellido")String nuevoApellido,
+            @RequestParam("img")String nuevoImg){
+        Persona persona= ipersonaService.searchPersona(id);
+        
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImg(nuevoImg);
+        ipersonaService.savePersona(persona);
+        return persona;
+    }
+    
     
 }
