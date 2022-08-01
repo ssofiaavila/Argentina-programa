92d4c60519760e21618fed3ec67b1455d71c8c51
soso <avilasofia076@gmail.com>
2022-07-31 22:50:08 -0300

    back

---
+++ Proyecto final/Back end/avila/src/main/java/com/sofiaavila/avila/AvilaApplication.java
@@ -0,0 +1,13 @@
+package com.sofiaavila.avila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvilaApplication.class, args);
+	}
+
+}
---
+++ Proyecto final/Back end/avila/src/test/java/com/sofiaavila/avila/AvilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofiaavila.avila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
