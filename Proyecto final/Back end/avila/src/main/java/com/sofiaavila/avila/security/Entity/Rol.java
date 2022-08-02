e7be4d3afea2048c825dadf85f0fb929a734368e
soso <avilasofia076@gmail.com>
2022-08-02 02:45:02 -0300

    security

--- Proyecto final/Back end/avila/src/main/java/com/sofiaavila/avila/security/Entity/Rol.java
+++ Proyecto final/Back end/avila/src/main/java/com/sofiaavila/avila/security/Entity/Rol.java
@@ -18,4 +18,38 @@ public class Rol {
     @NotNull
     @Enumerated(EnumType.STRING)
     private RolNombre rolNombre;
+    
+     //constructor
+    public Rol(){
+        
+    }
+    
+    public Rol(int id, RolNombre rolNombre) {
+        this.id = id;
+        this.rolNombre = rolNombre;
+    }
+    
+    //assets
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+    
+
+    
 }
+
+
+
