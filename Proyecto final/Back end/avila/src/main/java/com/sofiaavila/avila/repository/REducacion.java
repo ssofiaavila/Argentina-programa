341c4d2c84e41d234029fbb23a889cc04556db90
soso <avilasofia076@gmail.com>
2022-08-04 03:57:57 -0300

    Back de educación creada

---
+++ Proyecto final/Back end/avila/src/main/java/com/sofiaavila/avila/repository/REducacion.java
@@ -0,0 +1,13 @@
+
+package com.sofiaavila.avila.repository;
+
+import com.sofiaavila.avila.entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
