093adb676901675cea06da2e6d103856c84e3b1c
soso <avilasofia076@gmail.com>
2022-08-08 22:06:46 -0300

    Implementación de más campos en experiencia laboral

--- Proyecto final/Front end/src/app/components/experiencia/new-experiencia.component.ts
+++ Proyecto final/Front end/src/app/components/experiencia/new-experiencia.component.ts
@@ -11,6 +11,8 @@ import { SExperienciaService } from 'src/app/service/experiencia.service';
 export class NewExperienciaComponent implements OnInit {
   nombreE: string = '';
   descripcionE: string = '';
+  inicioE:number;
+  finE:number;
 
   constructor(private sExperiencia: SExperienciaService, private router: Router) { }
 
@@ -18,7 +20,7 @@ export class NewExperienciaComponent implements OnInit {
   }
 
   onCreate(): void {
-    const expe = new Experiencia(this.nombreE, this.descripcionE);
+    const expe = new Experiencia(this.nombreE, this.descripcionE,this.inicioE,this.finE);
     this.sExperiencia.save(expe).subscribe(
       data => {
         alert("Experiencia añadida");
--- Proyecto final/Front end/src/app/model/experiencia.ts
+++ Proyecto final/Front end/src/app/model/experiencia.ts
@@ -2,9 +2,13 @@ export class Experiencia {
     id? : number;
     nombreE : string;
     descripcionE : string;
+    inicioE: number;
+    finE:number;
 
-    constructor(nombreE: string, descripcionE: string){
+    constructor(nombreE: string, descripcionE: string, inicioE: number, finE: number) {
         this.nombreE = nombreE;
         this.descripcionE = descripcionE;
+        this.inicioE = inicioE;
+        this.finE= finE;
     }
 }
